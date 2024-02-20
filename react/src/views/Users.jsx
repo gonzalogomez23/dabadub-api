909e8faeaf747e43ba74e42834fcc710f9da76bb
gonzalogomez23 <gonzalo_ggm_@hotmail.com>
2024-02-20 19:20:07 +0100

    Firs commit (video min 2:02:00)

---
+++ react/src/views/Users.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import axiosClient from "../axios-client.js";
+
+export default function Users() {
+
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+            .then(({data}) => {
+                setLoading(false)
+                console.log(data)
+            })
+            .catch(() => {
+                setLoading(false)
+            })
+    }
+
+    return (
+    <>
+        Users
+    </>
+    )
+}
