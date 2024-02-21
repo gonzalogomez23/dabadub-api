53c72aa28c36b6cd2159cf249287a905bd0aac59
gonzalogomez23 <gonzalo_ggm_@hotmail.com>
2024-02-21 16:54:20 +0100

    Aplicación acabada sin notificaciones

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axiosClient from "../axios-client.js";
 
 export default function Users() {
@@ -10,12 +11,23 @@ export default function Users() {
         getUsers();
     }, [])
 
+    const onDelete = (u) => {
+        if(!window.confirm("Are you sure you want to delete this user?")) {
+            return
+        }
+
+        axiosClient.delete(`/users/${u.id}`)
+            .then(() => {
+                getUsers()
+            })
+    }
+
     const getUsers = () => {
         setLoading(true)
         axiosClient.get('/users')
             .then(({data}) => {
                 setLoading(false)
-                console.log(data)
+                setUsers(data.data)
             })
             .catch(() => {
                 setLoading(false)
@@ -24,7 +36,49 @@ export default function Users() {
 
     return (
     <>
-        Users
+        <div style={{display:'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+            <h1>Users</h1>
+            <Link to="/users/new" className="btn-add">Add new</Link> 
+        </div>
+        <div className="card animate fadeInDown">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Create Date</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                {loading &&
+                <tbody>
+                    <tr>
+                        <td colSpan="5" className="text-center">
+                            Loading...
+                        </td>
+                    </tr>
+                </tbody>
+                }
+                {!loading &&
+                <tbody>
+                    {users.map(u => (
+                        <tr>
+                            <td>{u.id}</td>
+                            <td>{u.name}</td>
+                            <td>{u.email}</td>
+                            <td>{u.created_at}</td>
+                            <td>
+                                <Link to={'/users/'+u.id} className="btn-edit">Edit</Link>
+                                <button onClick={en => onDelete(u)} className="btn-delete">Delete</button>
+                            </td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+                }
+            </table>
+        </div>
     </>
     )
 }
