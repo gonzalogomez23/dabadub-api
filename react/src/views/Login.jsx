f81af2e988896801db21905ecffd9866a30c1fe2
gonzalogomez23 <gonzalo_ggm_@hotmail.com>
2024-11-04 16:30:18 +0000

    Modified Login Page

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -53,7 +53,7 @@ export default function Login() {
                     <input ref={passwordRef} type="password" placeholder="Password"/>
                     <button className="btn btn-block">Login</button>
                     <p className="message">
-                        Not Registered? <Link to="/signup">Create an account</Link>
+                        Not Registered? <Link to="/signup">Create an accountt</Link>
                     </p>
                 </form>
             </div>
